e6e623e605b0a9ea93746f733623877f4692b389
DESKTOP-0PPT076\MSI <m.balev@editcreation.com>
2021-11-18 20:48:09 +0200

    header, no auth page

--- client/src/components/MainNavbar.js
+++ client/src/components/MainNavbar.js
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { AppBar, Toolbar, Box } from '@material-ui/core';
+import { AppBar, Toolbar, Box, Typography } from '@material-ui/core';
 import Logo from './Logo';
 import Message from '../components/message/Message';
 import MessageContext from '../contexts/MessageContext';
@@ -8,12 +8,19 @@ import MessageContext from '../contexts/MessageContext';
 const MainNavbar = (props) => {
   const messageContext = useContext(MessageContext);
 
-  return(
+  return (
     <AppBar
       elevation={0}
       {...props}
+      className="Header"
     >
-      <Toolbar sx={{ height: 64 }}>
+      <Box className="information-wrapper">
+        <Typography className="information-text">ул. "Антим I" №17</Typography>
+        <Typography className="information-text">02/ 935 6050</Typography>
+        <Typography className="information-text">02/ 988 3937</Typography>
+        <Typography className="information-text">rio_sofia_grad@mon.bg</Typography>
+      </Box>
+      <Toolbar sx={{ bgcolor: 'white' }}>
         <RouterLink to="/">
           <Logo />
         </RouterLink>
@@ -22,6 +29,8 @@ const MainNavbar = (props) => {
             <Message text={messageContext[0].text} status={messageContext[0].status}/>
         }
       </Toolbar>
+      <Toolbar style={{ minHeight: '50px' }}>
+     </Toolbar>
     </AppBar>
   );
 }
