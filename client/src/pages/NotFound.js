5ee4a4e9d7ae7cf5d6fd3491d274686c6b506459
DESKTOP-0PPT076\MSI <m.balev@editcreation.com>
2022-03-18 11:38:57 +0200

    404 page

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,55 +1,71 @@
 import { Helmet } from 'react-helmet';
+import { Link as RouterLink } from 'react-router-dom';
 import {
-  Box,
-  Container,
-  Typography
+    Box,
+    Container,
+    Typography,
+    Button
 } from '@material-ui/core';
+import {
+    Forward as ForwardIcon
+} from '@material-ui/icons';
 
 const NotFound = () => (
-  <>
-    <Helmet>
-      <title>404 | Material Kit</title>
-    </Helmet>
-    <Box
-      sx={{
-        backgroundColor: 'background.default',
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100%',
-        justifyContent: 'center'
-      }}
-    >
-      <Container maxWidth="md">
-        <Typography
-          align="center"
-          color="textPrimary"
-          variant="h1"
-        >
-          404: The page you are looking for isn’t here
-        </Typography>
-        <Typography
-          align="center"
-          color="textPrimary"
-          variant="subtitle2"
-        >
-          You either tried some shady route or you came here by mistake.
-          Whichever it is, try using the navigation
-        </Typography>
-        <Box sx={{ textAlign: 'center' }}>
-          <img
-            alt="Under development"
-            src="/static/images/undraw_page_not_found_su7k.svg"
-            style={{
-              marginTop: 50,
-              display: 'inline-block',
-              maxWidth: '100%',
-              width: 560
+    <>
+        <Helmet>
+            <title>404</title>
+        </Helmet>
+        <Box
+            sx={{
+                backgroundColor: 'background.default',
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+                justifyContent: 'center'
             }}
-          />
+        >
+            <Container maxWidth="md">
+                <Typography
+                    align="center"
+                    color="textPrimary"
+                    variant="h1"
+                >
+                    404: Страницата, която търсите, не съществува
+                </Typography>
+                {/* <Typography
+                    align="center"
+                    color="textPrimary"
+                    variant="subtitle2"
+                >
+                    You either tried some shady route or you came here by mistake.
+                    Whichever it is, try using the navigation
+                </Typography> */}
+                <Box sx={{ textAlign: 'center' }}>
+                    <img
+                        alt="Under development"
+                        src="/static/ruo.png"
+                        style={{
+                            marginTop: 10,
+                            display: 'inline-block',
+                            maxWidth: '100%',
+                            width: 360
+                        }}
+                    />
+                </Box>
+                <Box sx={{ textAlign: 'center' }}>
+                    <Button
+                        component={RouterLink}
+                        color="primary"
+                        variant="contained"
+                        to='/app/dashboard'
+                        endIcon={<ForwardIcon />}
+                    >
+                        Върни се към начало
+                    </Button>
+                </Box>
+            </Container>
         </Box>
-      </Container>
-    </Box>
-  </>
+    </>
 );
 
 export default NotFound;
