eb5c931d0fa8b9590a6f7b1f662319c200388b2a
DESKTOP-0PPT076\MSI <m.balev@editcreation.com>
2022-03-18 19:58:05 +0200

    clear interval in catch

--- client/src/App.js
+++ client/src/App.js
@@ -27,6 +27,7 @@ const App = () => {
           getUser();
         })
         .catch(err => {
+          clearInterval(interval);
           if (err.message === 'Unauthorized') {
             navigate('/login');
           }
