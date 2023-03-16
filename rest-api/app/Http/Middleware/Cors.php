f9c37bc3565364e37f514a0a390e7abc55ff50a1
Miroslav Balev <m.balev@editcreation.com>
2023-03-16 22:45:55 +0200

    update cors

--- rest-api/app/Http/Middleware/Cors.php
+++ rest-api/app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', 'http://192.168.0.161:3000')
+        ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
         ->header('Access-Control-Allow-Credentials', 'true')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With');
